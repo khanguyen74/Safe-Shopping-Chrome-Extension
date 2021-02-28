2d044503d9553697a58bb314328fe7bfbd306112
Thien Nhan Nguyen <thiennhanlqd@gmail.com>
2021-02-28 18:46:43 +0100

    Update

--- geturl.js
+++ geturl.js
@@ -1,12 +1,39 @@
+let serverLocationDom = document.getElementById('server-location');
+let organizationDom = document.getElementById('organization');
+
 onload = function(){
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
-        // since only one tab should be active and in the current window at once
-        // the return variable should only have one entry
-        var activeTab = tabs[0];
-        var activeTabId = activeTab.id; // or do whatever you need
-        var url = activeTabId.url;
-        input.value = url
-     });
+    //     // since only one tab should be active and in the current window at once
+    //     // the return variable should only have one entry
+    //     var activeTab = tabs[0];
+    //     var activeTabId = activeTab.id; // or do whatever you need
+    //     var url = activeTabId.url;
+    //     input.value = url
+    //  });
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url;
+        console.log(url);
+
+        var domain = (new URL(url));
+
+        domain.hostname.replace('www.','');
+
+        fetch(`http://localhost:8080/api/shop/${domain.hostname}`)
+            .then(r => r.json()).then(result => {
+                console.log(result)
+                
+                let whoRecord = result['WhoisRecord']
+                let organization = whoRecord['registrant'].organization
+                if (whoRecord && organization){
+                    var siteInfoDom = document.getElementById('site-info').style.visibility = "visible";
+                }
+                console.log(organization);
+                let serverLocation = whoRecord['registrant'].country
+                organizationDom.innerHTML = organization
+                serverLocationDom.innerHTML = serverLocation
+            })
+        // use `url` here inside the callback because it's asynchronous!
+    });
 }
--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 
-let whoisButton = document.getElementById('whois');
-let input = document.getElementById('urlInput'); 
-let serverLocationDom = document.getElementById('server-location');
-let organizationDom = document.getElementById('organization');
+// let whoisButton = document.getElementById('whois');
+let showHiddenButton = document.getElementById('hiddenElement');
 
-whoisButton.onclick = function(element){
-    let url = input.value;
-    if (url ===""){ 
-        alert('Please provide a URL');
-        return
-    }
-    fetch(`http://localhost:8080/api/shop/${url}`)
-    .then(r => r.json()).then(result => {
+// whoisButton.onclick = function(element){
+//     let url = input.value;
+//     if (url ===""){ 
+//         alert('Please provide a URL');
+//         return
+//     }
+//     fetch(`http://localhost:8080/api/shop/${url}`)
+//     .then(r => r.json()).then(result => {
         
-        let whoRecord = result['WhoisRecord']
-        let organization = whoRecord['registrant'].organization
-        if (whoRecord && organization){
-            var siteInfoDom = document.getElementById('site-info').style.visibility = "visible";
-        }
-        console.log(organization);
-        let serverLocation = whoRecord['registrant'].country
-        organizationDom.innerHTML = organization
-        serverLocationDom.innerHTML = serverLocation
-    })
+//         let whoRecord = result['WhoisRecord']
+//         let organization = whoRecord['registrant'].organization
+//         if (whoRecord && organization){
+//             var siteInfoDom = document.getElementById('site-info').style.visibility = "visible";
+//         }
+//         console.log(organization);
+//         let serverLocation = whoRecord['registrant'].country
+//         organizationDom.innerHTML = organization
+//         serverLocationDom.innerHTML = serverLocation
+//     })
+// }
+
+showHiddenButton.onclick = function(element){
 }
+
+
+
